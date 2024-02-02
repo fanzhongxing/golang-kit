2d736f309b72c039ac3ad5ff217936fa64ecfdee
fanzhongxing <2296494141@qq.com>
2024-02-02 15:00:30 +0800

    fix: modify create and add decode token

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/fanzhongxing/golang-kit
 
 go 1.20
 
-require github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+require github.com/golang-jwt/jwt/v5 v5.2.0
--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,21 +1,35 @@
 package auth
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"time"
 )
 
-var _DefaultSecret = "cbt0121"
-
 // CreateToken 生成 jwt token
-func CreateToken(data map[string]interface{}) (string, error) {
+func CreateToken(data map[string]interface{}, secret string) (string, error) {
 	claims := make(jwt.MapClaims)
 	claims = data
 	claims["iat"] = time.Now().Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString([]byte(_DefaultSecret))
+	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+// DecodeToken token 解码
+func DecodeToken(tokenStr string, secret string) (jwt.MapClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	tokenClaims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, errors.New("invalid type")
+	}
+	return tokenClaims, nil
 }
